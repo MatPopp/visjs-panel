f6b8dcfbee25f15bea86a6d5bbe9d966f3b1362f
Matthias Albert Popp <matthias.albert.popp@isc.fraunhofer.de>
2026-02-02 18:12:14 +0100

    graph_detail_tool_2 example

---
+++ examples/graph_detail_tool_2.py
@@ -0,0 +1,16 @@
+from visjs_panel import GraphDetailTool
+import panel as pn
+import json
+
+
+nodes = [
+        {"id": 1, "label": "Drop an Image File into the graph-widget", "shape":"ellipse", "color": "green"},
+    ]
+edges = [
+    ]
+
+
+if __name__ == "__main__":
+    graph_detail_panel = GraphDetailTool(nodes=json.dumps(nodes), edges=json.dumps(edges))
+
+    pn.serve(graph_detail_panel, threaded=True)
